aa04f512276ba159dd2f7b606a34e10d5925aaab
shypang <smartdragon417@gmail.com>
2023-03-19 23:11:51 +0900

    Edit: slice16.go

--- basic/slice/slice16.go
+++ basic/slice/slice16.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	slice := []int{1, 2, 3, 4, 5, 6}
+	//pop()함수랑 같은 기능이다.
 	t, slice := slice[len(slice)-1], slice[:len(slice)-1]
 	//6, 1,2,3,4,5
 	fmt.Println("t, slice", t, slice)
