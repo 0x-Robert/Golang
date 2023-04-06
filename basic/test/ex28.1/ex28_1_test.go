6f47cda09d07528f0462f2f0870865188aeac388
shypang <smartdragon417@gmail.com>
2023-04-06 23:40:24 +0900

    Add: testing and benchmark

--- basic/test/ex28.1/ex28_1_test.go
+++ basic/test/ex28.1/ex28_1_test.go
@@ -6,17 +6,12 @@ import (
 )
 
 func TestSquare1(t *testing.T) {
-	rst := square(9)
-	if rst != 81 {
-		t.Errorf("square(9) should be 81 but square(9 ) returns %d", rst)
-	}
+	assert := assert.New(t)                               //테스트 객체생성
+	assert.Equal(81, square(9), "square(9) should be 81") //테스트 함수 호출
+}
 
-// }
+func TestSquare2(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(9, square(3), "square(3) should be 9")
 
-// func TestSquare1(t *testing.T)  {
-// 	rst := square(9)
-// 	if rst != 81 {
-// 		t.Errorf("square(9) should be 81 but square(9 ) returns %d", rst)
-// 	}
-
-// }
+}
