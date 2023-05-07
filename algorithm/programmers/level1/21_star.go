e0a457c45b344192a33fbb460180c33009ab5cfa
shypang <smartdragon417@gmail.com>
2023-05-07 16:18:28 +0900

    Adding start

---
+++ algorithm/programmers/level1/21_star.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	for i := 0; i < b; i++ {
+		for j := 0; j < a; j++ {
+			fmt.Printf("%s", "*")
+		}
+		fmt.Println()
+	}
+}
