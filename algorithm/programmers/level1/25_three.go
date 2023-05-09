6c5041a32bf44978d9c2233c2f3723c7afbd1eab
root <root@DESKTOP-7T5N067.localdomain>
2023-05-09 23:11:19 +0900

    Adding

--- algorithm/programmers/level1/25_three.go
+++ algorithm/programmers/level1/25_three.go
@@ -8,9 +8,21 @@ number	result
 [-1, 1, -1, 1]	0
 */
 func solution(number []int) int {
-	return 0
+	var answer int
+	for i := 0; i < len(number)-2; i++ {
+		for j := i + 1; j < len(number)-1; j++ {
+			for k := j + 1; k < len(number); k++ {
+				if number[i]+number[j]+number[k] == 0 {
+					answer++
+				}
+			}
+		}
+	}
+
+	return answer
 }
 
 func main() {
-
+	number := []int{-2, 3, 0, 2, -5}
+	solution(number)
 }
