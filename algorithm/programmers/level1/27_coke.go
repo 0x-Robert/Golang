d4c3f812026d364bc185efa2fa6e9383d809cc55
shypang <smartdragon417@gmail.com>
2023-05-10 23:03:39 +0900

    coke

---
+++ algorithm/programmers/level1/27_coke.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func solution(a int, b int, n int) int {
+	var result int
+
+	
+
+	fmt.Println("result", result)
+	return 0
+}
+
+func main() {
+	a := 2
+	b := 1
+	n := 20
+
+}
