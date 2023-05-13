ecf031d89caae8c8d528bdf359b9b6fee10df3c6
shypang <smartdragon417@gmail.com>
2023-05-13 23:31:28 +0900

    Adding many

--- algorithm/programmers/level0/ex6/many.go
+++ algorithm/programmers/level0/ex6/many.go
@@ -1,13 +1,42 @@
 package main
 
-func solution(array []int) int {
-	
-	
+import "fmt"
 
-	return 0
+func solution(array []int) int {
+
+	//map 사용해서 int : int 의 키 밸류 선언
+	count := make(map[int]int)
+	fmt.Println("count", count)
+	for _, num := range array {
+		count[num]++
+	}
+
+	maxCount := 0
+	maxNum := 0
+
+	for num, cnt := range count {
+		if cnt > maxCount {
+			maxCount = cnt
+			maxNum = num
+		}
+	}
+
+	modeCount := 0
+	for _, cnt := range count {
+		if cnt == maxCount {
+			modeCount++
+		}
+	}
+	if modeCount > 1 {
+		maxNum = -1
+	}
+	//fmt.Println("maxNum", maxNum)
+	return maxNum
 }
 
 func main() {
-	arr := []int{1, 2, 3, 3, 3, 4}
+	arr := []int{1}
+	//arr := []int{1, 1, 2, 2}
+	//arr := []int{1, 2, 3, 3, 3, 4}
 	solution(arr)
 }
