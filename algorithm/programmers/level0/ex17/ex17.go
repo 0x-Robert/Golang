b5db7f6c974cedf284bd61611602cd8b91325f06
shypang <smartdragon417@gmail.com>
2023-05-22 07:36:25 +0900

    editing: mid value

--- algorithm/programmers/level0/ex17/ex17.go
+++ algorithm/programmers/level0/ex17/ex17.go
@@ -1,17 +1,16 @@
-package main 
+package main
 
 import (
-	"fmt"
 	"sort"
 )
 
 func solution(array []int) int {
 	sort.Sort(sort.IntSlice(array))
-	fmt.Println( array[len(array)-1 /2])
-	return array[len(array)-1/2]
+	// fmt.Println(array[len(array)/2])
+	return array[len(array)/2]
 }
 
-func main(){
-	array := []int{1,2,7,10,11}
+func main() {
+	array := []int{1, 2, 7, 10, 11}
 	solution(array)
 }
